fbaf21daf65af1b10cca74d1423ba82556b4d73e
khadija <ameksakhadija026@gmail.com>
2024-02-13 11:50:49 +0100

    create migration

--- database/migrations/2024_02_13_095600_create_messages_table.php
+++ database/migrations/2024_02_13_095600_create_messages_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
+            $table->text('content');
+            $table->integer('receiver');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
     }
